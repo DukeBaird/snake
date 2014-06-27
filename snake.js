b04752cabf5ac6b106334ccd9261f48610152181
Ben Craig <bencraig@storm.ca>
2014-06-27 15:15:27 -0400

    general outline

---
+++ snake.js
@@ -0,0 +1,18 @@
+var length = 1;
+var x[0] = 5;
+var y[0] = 5;
+var foodx = Math.Random();
+var foody = Math.Random();
+var alive = true;
+var direction = 'u';
+
+while (alive) {
+
+move(direction);
+
+checkCollisions();
+checkFood();
+
+}
+
+
